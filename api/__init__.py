f30a93d56980679674acb9caf2a6802fcd040c1f
durgavikas <durgavikas6@gmail.com>
2025-04-30 21:05:13 +0530

    Logging Configs

---
+++ api/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
+logger.info('Basic Logging Config is added at API Service')
