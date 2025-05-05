2105a645eb4a4bf429c99ea3a97731d23084a63b
durgavikas <durgavikas6@gmail.com>
2025-05-05 18:00:00 +0530

    Add heart rate, heart rate variability, and RR interval calculations to ECG processor

--- processing_engine/ecg_processor.py
+++ processing_engine/ecg_processor.py
@@ -1,4 +1,5 @@
 import neurokit2 as nk
+import numpy as np
 import pandas as pd
 
 
@@ -35,3 +36,73 @@ class ECGProcessor:
         signals,info = nk.ecg_process(ecg_signal, sampling_rate=self.sampling_rate)
         ecg_clean = signals["ECG_Clean"]
         return ecg_clean
+
+    def hr(self, ecg_signal):
+        """
+        Calculate the Heart Rate (HR) from the ECG signal.
+
+        Parameters
+        ----------
+        ecg_signal : array
+            The ECG signal to calculate HR.
+
+        Returns
+        -------
+        float
+            Heart rate value.
+        """
+        # HR
+        ecg_clean = self.preprocessing_ecg(ecg_signal)
+        signals, info = nk.ecg_peaks(ecg_clean,sampling_rate=self.sampling_rate)
+        heart_rate_mean = np.mean(signals["ECG_Rate"])
+        hr = round(heart_rate_mean)
+
+        return hr
+
+    def hrv(self , ecg_signal):
+        """
+        Calculate the Heart Rate Variability (HRV) from the ECG signal.
+
+        Parameters
+        ----------
+        ecg_signal : array
+            The ECG signal to calculate HRV.
+
+        Returns
+        -------
+        float
+            HRV value.
+        """
+        # HRV
+        ecg_clean = self.preprocessing_ecg(ecg_signal)
+        signals, info = nk.ecg_peaks(ecg_clean,sampling_rate=self.sampling_rate, correct_artifacts=True)
+        hrv_details = nk.hrv(signals, sampling_rate=self.sampling_rate)
+        HRV_MeanNN = hrv_details["HRV_MeanNN"].iloc[0]
+        hrv = round(HRV_MeanNN)
+
+        return hrv
+
+    def rr(self, ecg_signal):
+        """
+        Calculate the RR intervals from the ECG signal.
+
+        Parameters
+        ----------
+        ecg_signal : array
+            The ECG signal to calculate RR intervals.
+
+        Returns
+        -------
+        float
+            RR interval value.
+        """
+        # RR
+        ecg_clean = self.preprocessing_ecg(ecg_signal)
+        peaks = nk.ecg_peaks(ecg_clean, sampling_rate=self.sampling_rate, correct_artifacts=True)
+        ecg_rate = nk.ecg_rate(peaks, sampling_rate=self.sampling_rate, desired_length=len(ecg_clean))
+        edr = nk.ecg_rsp(ecg_rate, sampling_rate=self.sampling_rate)
+        rr_signals, info = nk.rsp_process(edr, sampling_rate=self.sampling_rate)
+        rr_mean = np.mean(rr_signals["RSP_Rate"])
+        rr = round(rr_mean)
+
+        return rr
